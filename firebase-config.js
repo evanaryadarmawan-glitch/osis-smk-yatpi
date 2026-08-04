792984725809dc922c41ef3894e3a22948e1ccd3
evanaryadarmawan-glitch <evanaryadarmawan@gmail.com>
2026-08-05 02:14:02 +0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Konfigurasi Firebase SDK v9 Compat
 const firebaseConfig = {
   apiKey: "AIzaSyB78x1-kamlwnsjmVHNJQYW5ADV2assCDA",
   authDomain: "osis-smk-yatpi-6fa18.firebaseapp.com",
@@ -16,6 +9,8 @@ const firebaseConfig = {
   measurementId: "G-PPKLKF4YSW"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Inisialisasi Firebase (Compat Mode)
+firebase.initializeApp(firebaseConfig);
+
+// Inisialisasi Database agar dapat dipanggil secara global di index.html & admin.html
+const database = firebase.database();
